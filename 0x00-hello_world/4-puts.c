dcea7e53740d5fe9dd69e0d0972987671feb95dd
70SS4M <hussam.mahmoud93@gmail.com>
2023-07-13 15:44:23 -0700

    Hello World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: A C programme.
+ * Return: Always 0 (Succesful)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
