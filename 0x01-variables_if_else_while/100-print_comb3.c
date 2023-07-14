5b6baa3a533c344eb05d3d5da4448dd4570df84f
70SS4M <hussam.mahmoud93@gmail.com>
2023-07-14 13:11:24 -0700

    Variables - if - else - while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,11 +17,14 @@ int main(void)
 		num2 = num1 + 1;
 		while (num2 <= 9)
 		{
-		      	putchar(num1 + 48);
+		        putchar(num1 + 48);
 			putchar(num2 + 48);
-			putchar(',');
-			putchar(' ');
-			++num2;
+			if (num1 +num2 != 17)
+			  {
+			    	putchar(',');
+				putchar(' ');
+			  }
+		       	++num2;
 		}
 		++num1;
 	}
